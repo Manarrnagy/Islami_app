8e65a82b1628be67732693f6caa9dc6a5e2dd132
manarrnagy <manarrnagy@gmail.com>
2023-02-02 03:36:12 +0200

    Fixed sebha

--- app/src/main/java/com/route/islami/adapters/SuraNameAdapter.kt
+++ app/src/main/java/com/route/islami/adapters/SuraNameAdapter.kt
@@ -7,9 +7,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.route.islami.R
 
-class SuraNameAdapter(val items: List<String> ) :RecyclerView.Adapter<SuraNameAdapter.ViewHolder>(){
+class SuraNameAdapter(val items: List<String> , val lines: List<Int>) :RecyclerView.Adapter<SuraNameAdapter.ViewHolder>(){
     class ViewHolder (itemView: View): RecyclerView.ViewHolder(itemView){
         val  suraNameTv = itemView.findViewById<TextView>(R.id.suraNameTv)
+        val  suraLinesTv = itemView.findViewById<TextView>(R.id.suraLinesTv)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,13 +24,18 @@ class SuraNameAdapter(val items: List<String> ) :RecyclerView.Adapter<SuraNameAd
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.suraNameTv.text = items.get(position)
+        holder.suraLinesTv.text = lines.get(position).toString()
         holder.suraNameTv.setOnClickListener {
+            onItemClick?.onSuraNameClick(items.get(position),position)
+        }
+        holder.suraLinesTv.setOnClickListener {
             onItemClick?.onSuraNameClick(items.get(position),position)
         }
     }
     var onItemClick : OnItemClick?= null
     interface OnItemClick{
         fun onSuraNameClick(sura:String, postion: Int)
+        fun onSuraLineClick(sura:String, postion: Int)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/route/islami/fragments/QuraanFragment.kt
+++ app/src/main/java/com/route/islami/fragments/QuraanFragment.kt
@@ -25,6 +25,22 @@ class QuraanFragment : Fragment() {
         ,"المطفّفين","الإنشقاق","البروج","الطارق","الأعلى","الغاشية","الفجر","البلد","الشمس","الليل","الضحى","الشرح"
         ,"التين","العلق","القدر","البينة","الزلزلة","العاديات","القارعة","التكاثر","العصر",
         "الهمزة","الفيل","قريش","الماعون","الكوثر","الكافرون","النصر","المسد","الإخلاص","الفلق","الناس")
+    val ayatNumbers = listOf<Int>(
+        7,286       ,200,176,   120,165,    206,75,     129,109,
+        123,111     ,43,52,     99,128,     111,110,    98,135,
+        112,78,     118,64,     77,227,     93,88,      69,60,
+        34,30,      73,54,      45,83,      182,88,     75,85,
+        54,53,      89,59,      37,35,      38,29       ,18,45,
+        60,49,      62,55,      78,96,      29,22,      24,13,
+        14,11,      11,18,      12,12,      30,52,      52,44,
+        28,28,      20,56,      40,31,      50,40       ,46,42,
+        29,19,      36,25,      22,17,      19,26,      30,20,
+        15,21,      11,8,       8,19        ,5,8,       8,11,
+        11,8        ,3,9,       5,4,        7,3,        6,3,
+        5,4         ,5,6
+
+
+    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,9 +52,17 @@ class QuraanFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = view.findViewById<RecyclerView>(R.id.suraRV)
-        adapter = SuraNameAdapter(suraNames)
+        adapter = SuraNameAdapter(suraNames,ayatNumbers)
         adapter.onItemClick = object : SuraNameAdapter.OnItemClick{
             override fun onSuraNameClick(sura: String, postion: Int) {
+                val fileName = "${postion+1}.txt"
+                val intent = Intent(requireActivity(), SuraDetailsActivity::class.java)
+                intent.putExtra("fileName","${postion+1}.txt")
+                intent.putExtra("suraName",sura)
+                startActivity(intent)
+            }
+
+            override fun onSuraLineClick(sura: String, postion: Int) {
                 val fileName = "${postion+1}.txt"
                 val intent = Intent(requireActivity(), SuraDetailsActivity::class.java)
                 intent.putExtra("fileName","${postion+1}.txt")
